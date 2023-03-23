f603d4a893b39b3b827c1924d64e52009636ceb4
Otaiki1 <otaikisadiq2804@gmail.com>
2023-03-23 12:44:34 +0100

    refac: add modal functionalites

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,12 @@
 import { useLiveQuery } from 'dexie-react-hooks'
 import getPhotoUrl from 'get-photo-url'
+import { useState } from 'react'
 import { db } from '../dexie'
 import Modal from './Modal'
 
 const Gallery = () => {
   const allPhotos = useLiveQuery(() => db.gallery.toArray(), [])
+  const [showModal, setShowModal] = useState(false);
 
   const addPhoto = async () => {
     db.gallery.add({
@@ -14,28 +16,31 @@ const Gallery = () => {
 
   const removePhoto = (id) => {
     db.gallery.delete(id)
+    setShowModal(false)
   }
 
+
   return (
-    <>
+    <div>
       <input type="file" accept="image/*" name="photo" id="addPhotoInput" />
       <label htmlFor="addPhotoInput" onClick={addPhoto}>
         <i className="add-photo-button fas fa-plus-square" />
       </label>
 
       <section className="gallery">
-        <Modal />
+        
         {!allPhotos && <p>Loading...</p>}
         {allPhotos?.map((photo) => (
           <div className="item" key={photo.id}>
             <img src={photo.url} className="item-image" alt="" />
-            <button className="delete-button" onClick={() => removePhoto(photo.id)}>
+            <button className="delete-button" onClick={() => setShowModal(true)}>
               Delete
             </button>
+            {showModal && <Modal cancelHandler={() => setShowModal(false)} confirmHandler={() => removePhoto(photo.id)} />}
           </div>
         ))}
       </section>
-    </>
+    </div>
   )
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,10 @@
-export default function Modal(){
+export default function Modal({confirmHandler, cancelHandler}){
     return(
         <div className="modal">
             <h1>Are you sure you want to delete?</h1>
             <div className="modal-buttons">
-                <button className="modal-confirm">Confirm</button>
-                <button className="modal-cancel">Cancel</button>
+                <button className="modal-confirm" onClick={confirmHandler}>Confirm</button>
+                <button className="modal-cancel" onClick={cancelHandler}>Cancel</button>
             </div>
         </div>
     )   
